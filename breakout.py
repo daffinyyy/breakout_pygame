c556f0322121c9c203ac3457466a57c9f868d5a5
Aline Daffiny Ferreira Gomes <adfg.snf23@uea.edu.br>
2023-12-04 21:26:44 -0400

    destroy bricks, score and game over

--- breakout.py
+++ breakout.py
@@ -1,28 +1,9 @@
 import pygame
-import random
-import numpy
+from status import *
 from rectangle import *
+from constants import *
 
 pygame.init()
-
-COLOR_BLACK = (0, 0, 0)
-COLOR_WHITE = (255, 255, 255)
-COLOR_BLUE = (0, 0, 255)
-COLOR_ORANGE = (255, 165, 0)
-COLOR_GREEN = (0, 255, 0)
-
-SCREEN_WIDTH = 720
-SCREEN_HEIGHT = 900
-
-PLAYER_WIDTH = 100
-PLAYER_HEIGHT = 20
-
-BALL_SIZE = 20
-
-FIRST_BRICK_X = 25
-BRICK_WIDTH = 90
-BRICK_HEIGHT = 20
-BRICK_OFFSET = 24
 
 bricks = []
 
@@ -30,16 +11,11 @@ size = (SCREEN_WIDTH, SCREEN_HEIGHT)
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Breakout - PyGame Edition - 2023-11-30")
 
-# score text
-score_font = pygame.font.Font('assets/PressStart2P.ttf', 44)
-score_text = score_font.render('00', True, COLOR_WHITE, COLOR_BLACK)
-score_text_rect = score_text.get_rect()
-score_text_rect.center = (SCREEN_WIDTH - score_text_rect.width,
-                          score_text_rect.height)
+FONT = pygame.font.Font('assets/PressStart2P.ttf', 44)
 
 # player 1
 player_1 = Player("assets/player.png", "h", PLAYER_WIDTH, PLAYER_HEIGHT, 300, SCREEN_HEIGHT - 100, 8)
-ball = Ball("assets/player.png", BALL_SIZE, 300, SCREEN_HEIGHT / 2, 8, -30)
+ball = Ball("assets/player.png", BALL_SIZE, BALL_START_X, BALL_START_Y, 8, -30)
 
 # brick
 color = COLOR_BLUE
@@ -56,6 +32,7 @@ for i in range(0, 6):
         bricks[i].append(Brick("assets/player.png", "h", BRICK_WIDTH, BRICK_HEIGHT, brick_x, row_y, color))
         brick_x += BRICK_WIDTH + BRICK_OFFSET
     row_y += 30
+Game_Status.bricks_alive = 36
 
 # victory
 victory = False
@@ -83,16 +60,26 @@ while game_loop:
                 player_1.move_right = False
 
     # checking the victory condition
-    if not victory:
-
+    if not Game_Status.bricks_alive == 0 and Game_Status.player_lives > 0:
         # clear screen
         screen.fill(COLOR_BLACK)
 
         player_1.move(0, SCREEN_WIDTH - PLAYER_WIDTH)
         ball.move(0, SCREEN_WIDTH - BALL_SIZE, 0, SCREEN_HEIGHT - BALL_SIZE)
 
+        lives_text = FONT.render(str(Game_Status.player_lives), True, COLOR_WHITE, COLOR_BLACK)
+        lives_text_rect = lives_text.get_rect()
+        lives_text_rect.center = (lives_text_rect.width,
+                                  lives_text_rect.height)
+
+        score_text = FONT.render(str(Game_Status.player_score), True, COLOR_WHITE, COLOR_BLACK)
+        score_text_rect = score_text.get_rect()
+        score_text_rect.center = (SCREEN_WIDTH - score_text_rect.width,
+                                  score_text_rect.height)
+
         # drawing objects
         screen.blit(score_text, score_text_rect)
+        screen.blit(lives_text, lives_text_rect)
         screen.blit(ball.game_object, (ball.x, ball.y))
         screen.blit(player_1.game_object, (player_1.x, player_1.y))  # SCREEN_HEIGHT - 100))
 
@@ -100,6 +87,47 @@ while game_loop:
             for y in range(0, 6):
                 if (not bricks[x][y].destroyed):
                     screen.blit(bricks[x][y].game_object, (bricks[x][y].x, bricks[x][y].y))
+    elif Game_Status.player_lives > 0:
+        # clear screen
+        screen.fill(COLOR_BLACK)
+
+        victory_text = FONT.render("YOU WIN", True, COLOR_WHITE, COLOR_BLACK)
+        victory_text_rect = victory_text.get_rect()
+        victory_text_rect.center = (SCREEN_WIDTH / 2,
+                                    SCREEN_HEIGHT / 2 - victory_text_rect.height * 4)
+        screen.blit(victory_text, victory_text_rect)
+
+        score_text = FONT.render("SCORE:" + str(Game_Status.player_score), True, COLOR_WHITE, COLOR_BLACK)
+        score_text_rect = score_text.get_rect()
+        score_text_rect.center = (score_text_rect.width / 2 + 30,
+                                  victory_text_rect.bottom + victory_text_rect.height * 2)
+        screen.blit(score_text, score_text_rect)
+
+        extra_score_text = FONT.render("EXTRA LIVES: " + str(Game_Status.player_lives - 1), True, COLOR_WHITE,
+                                       COLOR_BLACK)
+        extra_score_text_rect = extra_score_text.get_rect()
+        extra_score_text_rect.center = (extra_score_text_rect.width / 2 + 30,
+                                        score_text_rect.bottom + score_text_rect.height)
+        screen.blit(extra_score_text, extra_score_text_rect)
+
+        total_score_text = FONT.render(
+            "TOTAL SCORE:" + str(Game_Status.player_score + (Game_Status.player_lives - 1) * 50), True, COLOR_WHITE,
+            COLOR_BLACK)
+        total_score_text_rect = total_score_text.get_rect()
+        total_score_text_rect.center = (total_score_text_rect.width / 2 + 30,
+                                        extra_score_text_rect.bottom + extra_score_text_rect.height * 2)
+        screen.blit(total_score_text, total_score_text_rect)
+
+    else:
+        # clear screen
+        screen.fill(COLOR_BLACK)
+
+        defeat_text = FONT.render('YOU LOSE', True, COLOR_WHITE, COLOR_BLACK)
+        defeat_text_rect = defeat_text.get_rect()
+        defeat_text_rect.center = (SCREEN_WIDTH / 2,
+                                   SCREEN_HEIGHT / 2)
+
+        screen.blit(defeat_text, defeat_text_rect)
 
     # update screen
     pygame.display.flip()
--- engine.py
+++ engine.py
@@ -2,25 +2,26 @@
 class Collider():
     active_colliders = []
 
-    def __init__(self):
-        self.cooldown = 0 # used for problem with multiple collision
+    def __init__(self, cooldown):
+        self.cooldown = cooldown  # used for problem with multiple collision
+        self.current_cooldown = 0
 
     def colision(collider):
-        if(collider.cooldown == 0):
-            for active_collider in Collider.active_colliders: # check all coliders in map
-                if(collider != active_collider):
-                    if(active_collider.check_colision(collider)): # if colliding returns the collider
-                        collider.cooldown = 2
+        if (collider.current_cooldown == 0):
+            for active_collider in Collider.active_colliders:  # check all coliders in map
+                if (collider != active_collider):
+                    if (active_collider.check_colision(collider)):  # if colliding returns the collider
+                        collider.current_cooldown = collider.cooldown
                         return active_collider
         else:
-            collider.cooldown -= 1
+            collider.current_cooldown -= 1
         return None
 
 
 # Rectangular collider
 class Box_Collider(Collider):
-    def __init__(self, x, y, width, height, object):
-        super().__init__()
+    def __init__(self, x, y, width, height, object, cooldown):
+        super().__init__(cooldown)
         self.x = x
         self.y = y
         self.width = width
